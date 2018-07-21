e723dab4b224c890519188aa5d21ac94b1247e8f
prasangafdz@outlook.com <LykanHypersp0rt@#>
2018-07-21 15:38:44 +0530

    Included creation of Parcels in sender's view

--- RapidDelivery mid 2018/RapidDelivery/app/Http/Controllers/SenderParcelsController.php
+++ RapidDelivery mid 2018/RapidDelivery/app/Http/Controllers/SenderParcelsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Parcel;
 use App\SenderParcel;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,9 @@ class SenderParcelsController extends Controller
      */
     public function index()
     {
-        //
+        $parcels = Parcel::all();
+        return view('senderparcels.index', ['parcels'=>$parcels]);
+       // print($parcels);
     }
 
     /**
@@ -24,7 +27,7 @@ class SenderParcelsController extends Controller
      */
     public function create()
     {
-        //
+        return view('senderparcels.create');
     }
 
     /**
@@ -35,7 +38,34 @@ class SenderParcelsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request);
+        // if(Auth::check()){
+            $parcel =Parcel::create([
+                'item'=>$request->input('item'),
+                'pickup_address'=>$request->input('pickup_address'),
+                'pickup_state'=>$request->input('pickup_state'),
+                'delivery_address'=>$request->input('delivery_address'),
+                'delivery_state'=>$request->input('delivery_state'),               
+                'status'=>'available',               
+            ]);
+            $parcel_sender =SenderParcel::create([
+                'sender_id'=>'1',//Change later
+                'parcel_id'=>$parcel->id,             
+            ]);
+
+            if($parcel && $parcel_sender){
+                return redirect()->route('parcels.show', ['parcel'=>$parcel->id])
+                ->with('success', 'Parcel added successfully');
+            // if ($parcel_sender) {
+            //     print("Done");
+            
+            }
+       // return redirect()->route('senderparcels.show', 5)->with('success', 'Parcel added successfully');
+
+        // }
+        // else
+           // return('error');
+            //return back()->withInput()->with('error', 'Error saving data');//Not working
     }
 
     /**
@@ -44,18 +74,22 @@ class SenderParcelsController extends Controller
      * @param  \App\SenderParcel  $senderParcel
      * @return \Illuminate\Http\Response
      */
-    public function show(SenderParcel $senderParcel)
+    public function show(Parcel $parcel)
     {
-        //
+        $dd= Parcel::find($parcel->id);//Check the migration
+       //return view('userparcels.show', ['parcel'=>$dd]);
+         print($dd);
+        //return view('senderparcels.show', ['parcel'=>$dd]);
+
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\SenderParcel  $senderParcel
+     * @param  \App\Parcel  $parcel
      * @return \Illuminate\Http\Response
      */
-    public function edit(SenderParcel $senderParcel)
+    public function edit(Parcel $parcel)
     {
         //
     }
@@ -64,10 +98,10 @@ class SenderParcelsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\SenderParcel  $senderParcel
+     * @param  \App\Parcel  $parcel
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, SenderParcel $senderParcel)
+    public function update(Request $request, Parcel $parcel)
     {
         //
     }
@@ -75,11 +109,56 @@ class SenderParcelsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\SenderParcel  $senderParcel
+     * @param  \App\Parcel  $parcel
      * @return \Illuminate\Http\Response
      */
-    public function destroy(SenderParcel $senderParcel)
+    public function destroy(Parcel $parcel)
     {
         //
     }
 }
+
+    //Generated code
+//     public function show(SenderParcel $senderParcel)
+//     {
+//        //   $dd= Parcel::find($parcel->id);//Check the migration
+//        // //return view('userparcels.show', ['parcel'=>$dd]);
+//        // //  print($dd);
+//        //  return view('senderparcels.show', ['parcel'=>$dd]);
+//         print("arg");
+//     }
+
+//     /**
+//      * Show the form for editing the specified resource.
+//      *
+//      * @param  \App\SenderParcel  $senderParcel
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function edit(SenderParcel $senderParcel)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Update the specified resource in storage.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  \App\SenderParcel  $senderParcel
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function update(Request $request, SenderParcel $senderParcel)
+//     {
+//         //
+//     }
+
+//     /**
+//      * Remove the specified resource from storage.
+//      *
+//      * @param  \App\SenderParcel  $senderParcel
+//      * @return \Illuminate\Http\Response
+//      */
+//     public function destroy(SenderParcel $senderParcel)
+//     {
+//         //
+//     }
+// }
